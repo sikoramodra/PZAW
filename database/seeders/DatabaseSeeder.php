0c6cc5f0033165129ac09e98c3c6ad6d720ce2c9
sikoramodra <wojciechmodro@gmail.com>
2023-10-18 19:40:29 +0200

    zad5 + changed file permissions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Publication;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +21,24 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+
+        Publication::create([
+            'title' => 'Advanced Networking in GNU/Linux',
+            'content' => 'Dive into the intricacies of networking in GNU/Linux with this advanced guide. It covers topics such as IP addressing, routing, VLANs, and network troubleshooting. Additionally, it explores advanced networking protocols and services like VPNs and DNS. Whether you are a network administrator or a seasoned GNU/Linux user, this publication will equip you with the knowledge and skills to handle complex networking scenarios.',
+            'author' => 'Jennifer Smith',
+        ]);
+
+        Publication::create([
+            'title' => 'Containerization with Docker in GNU/Linux',
+            'content' => 'Explore the world of containerization using Docker in the GNU/Linux environment. This guide provides a hands-on approach to creating, deploying, and managing containers. It covers topics like Docker images, containers, and Docker Compose for multi-container applications. Whether you are a developer or an administrator, this publication will help you leverage the power of Docker for efficient application deployment.',
+            'author' => 'Kevin Johnson',
+        ]);
+
+        Publication::create([
+            'title' => 'Data Backup and Recovery in GNU/Linux',
+            'content' => 'Protect your data with effective backup and recovery strategies in GNU/Linux. This guide provides step-by-step instructions for setting up automated backups, both locally and in the cloud. It covers tools like rsync, tar, and cloud storage services for secure data storage. Additionally, it offers insights into disaster recovery planning. By following the practices outlined in this publication, you can ensure the safety and availability of your critical data in GNU/Linux.',
+            'author' => 'Emily Davis',
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,9 @@
 use App\Models\Publication;
 use Illuminate\Support\Facades\Route;
 
-// https://tinyurl.com/bdfra6pc
-
 Route::get('/', function(){
     return view('home', [
-        'date' => now(),
+        'publication' => Publication::orderBy('created_at', 'desc')->first(),
     ]);
 })->name('home');
 
@@ -16,33 +14,7 @@ Route::get('/about-us', function(){
 })->name('about_us');
 
 
-$publications = [
-    new Publication([
-        'title' => 'Introduction to GNU/Linux',
-        'content' => 'This publication provides an in-depth overview of the GNU/Linux operating system. It covers topics such as the history of GNU/Linux, its architecture, file system, package management, and more. Additionally, it explores the various distributions available and their respective strengths. Whether you are a beginner or an experienced user, this publication is a valuable resource for understanding the core concepts of GNU/Linux.',
-        'author' => 'Alex Johnson'
-    ]),
-    new Publication([
-        'title' => 'Command Line Essentials in GNU/Linux',
-        'content' => 'In this comprehensive guide, you will learn the fundamental command line tools and techniques in GNU/Linux. The content includes practical examples and exercises to help you gain proficiency in using the command line interface. Topics covered include file management, process control, text processing, and more. By the end of this publication, you will have a solid foundation in navigating and utilizing the command line in GNU/Linux.',
-        'author' => 'Sarah Williams'
-    ]),
-    new Publication([
-        'title' => 'Managing Services in GNU/Linux',
-        'content' => 'This publication covers how to effectively manage services and daemons in a GNU/Linux system. It provides step-by-step instructions for tasks such as starting, stopping, and restarting services. Additionally, it discusses advanced topics like service dependencies and configuration management. Whether you are a system administrator or an enthusiast, this guide will equip you with the knowledge and skills to efficiently manage services in GNU/Linux.',
-        'author' => 'Michael Brown'
-    ]),
-    new Publication([
-        'title' => 'GNU/Linux Security Best Practices',
-        'content' => 'Explore a comprehensive set of best practices for securing your GNU/Linux system against common threats. This publication delves into topics such as user authentication, firewall configuration, intrusion detection, and more. It also provides practical tips for maintaining a secure system over time. By following these guidelines, you can significantly enhance the security posture of your GNU/Linux environment.',
-        'author' => 'Jessica Davis'
-    ]),
-    new Publication([
-        'title' => 'Customizing Desktop Environments in GNU/Linux',
-        'content' => 'Learn how to personalize and enhance your desktop experience in GNU/Linux. This guide covers a wide range of customization options, including themes, icons, desktop environments, and window managers. It also provides tips for optimizing system performance while customizing the desktop. Whether you prefer a sleek and minimalistic setup or a vibrant and feature-rich environment, this publication will help you achieve the perfect desktop configuration in GNU/Linux.',
-        'author' => 'David Wilson'
-    ])
-];
+$publications = Publication::orderBy('created_at', 'desc')->get();
 
 
 Route::get('/publications', function() use($publications){

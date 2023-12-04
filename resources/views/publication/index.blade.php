1836ba9883b89c52925108517ef82dfdd16d9507
sikoramodra <wojciechmodro@gmail.com>
2023-12-04 20:02:26 +0100

    fix #1; feedback alert for delete operation

--- resources/views/publication/index.blade.php
+++ resources/views/publication/index.blade.php
@@ -4,6 +4,7 @@
 
 @section('content')
     <h1 class="heading">publications</h1>
+    <x-alerts></x-alerts>
 
     <div class="grid grid-cols-2 md:grid-cols-3 gap-4 p-2">
         @foreach ($publications as $publication)

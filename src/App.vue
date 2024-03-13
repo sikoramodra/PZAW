d99ec3b28950fe2d93918bf4f2c0a9967c319a03
sikoramodra <wojciechmodro@gmail.com>
2024-03-13 16:29:34 +0100

    zad18 - WIP

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,79 @@
 <script setup>
-import { reactive } from 'vue';
-import TodoItem from './components/todoItem.vue';
+import { reactive, ref } from 'vue';
+import TodoList from './components/todoList.vue';
 
 const todo = reactive([
   {
-    text: 'Zrobić matmę',
-    finished: false,
+    name: 'porządki',
+    tasks: [
+      {
+        text: 'Odkurzyć pokój',
+        finished: false,
+      },
+      {
+        text: 'Wynieść śmieci',
+        finished: true,
+      },
+    ],
   },
   {
-    text: 'Odkurzyć pokój',
-    finished: false,
-  },
-  {
-    text: 'Wynieść śmieci',
-    finished: true,
+    name: 'szkoła',
+    tasks: [
+      {
+        text: 'Zrobić matmę',
+        finished: false,
+      },
+      {
+        text: 'Perzeczytać lekturę',
+        finished: false,
+      },
+    ],
   },
 ]);
+
+const isActive = ref(false);
 </script>
 
 <template>
-  <ul>
-    <template v-for="task in todo">
-      <TodoItem :text="task.text" :finished="task.finished" @changed="(task) => task.finished = !task.finished" />
-    </template>
-  </ul>
-  <div v-if="todo.every((task) => task.finished)">All tasks are finished!</div>
+  <h2 class="accordion" :class="{ active: isActive }" @click="isActive = !isActive">Tasks</h2>
+  <div v-for="task in todo" class="panel" v-bind:style="{ display: isActive ? 'block' : 'none' }">
+    <TodoList >{{ task.name }}</TodoList>
+  </div>
 </template>
 
+<style>
+.accordion {
+  background-color: #eee;
+  color: #444;
+  cursor: pointer;
+  padding: 18px;
+  width: calc(100% - 36px);
+  text-align: left;
+  border: none;
+  outline: none;
+  transition: 0.4s;
+}
 
+.active, .accordion:hover {
+  background-color: #ccc;
+}
+
+.panel {
+  padding: 0 18px;
+  background-color: white;
+  display: none;
+  overflow: hidden;
+}
+
+.accordion:after {
+  content: '\02795';
+  font-size: 13px;
+  color: #777;
+  float: right;
+  margin-left: 5px;
+}
+
+.active:after {
+  content: "\2796";
+}
+</style>
---
+++ src/components/todoList.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue';
+
+const isActive = ref(false);
+</script>
+
+<template>
+    <h3 class="accordion" :class="{ active: isActive }" @click="isActive = !isActive"><slot /></h3>
+    <div class="panel" v-bind:style="{ display: isActive ? 'block' : 'none' }">
+        
+    </div>
+</template>

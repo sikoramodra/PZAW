b5aa298fffb431c4dde1013d375b10d5905811ae
sikoramodra <wojciechmodro@gmail.com>
2024-10-02 09:24:58 +0200

    useRef

--- src/Sci.js
+++ src/Sci.js
@@ -1,9 +1,25 @@
+import { useRef } from "react";
+
 function Sci() {
+	const inputRef = useRef()
+	
+	const handleButton = () => {
+		console.log("HI");
+		console.log(inputRef.current.value)
+	}
+
+	const handleInputText = (e) => {
+		console.log(e.target.value);
+	}
+
 	return (
 		<div>
 			Sci
+			<input type="button" value="asd" onClick={handleButton}></input>
+
+			<input ref={inputRef} type="text" onChange={handleInputText}></input>
 		</div>
-	)
+	);
 }
 
 export default Sci;

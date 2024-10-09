d9033c15b040547edc57e40928ffce88a3b94ec8
sikoramodra <wojciechmodro@gmail.com>
2024-10-09 09:27:16 +0200

    form

--- src/Component.js
+++ src/Component.js
@@ -1,31 +1,70 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { useEffect } from 'react'
 import './Component.css'
 
 function Component() {
-  const [name, setName] = useState('sikoramodra')
-  const [num, setNum] = useState(0)
+  const text1 = useRef()
+  const text2 = useRef()
+  const comp = useRef()
+  //const [text1, setText1] = useState()
+  //const [text2, setText2] = useState()
+  //const [comp, setComp] = useState()
 
-  useEffect(() => {
-    console.log('useEffect is called')
-  }, [])
+  const [col, setCol] = useState()
 
-  const myStyle = { color: '#0F0', fontSize: '32px' }
+  const [style, setStyle] = useState({})
 
-  const buttonClick = () => {
-    console.log(name)
-    setName('asd')
-    console.log(name)
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    setStyle({
+      backgroundColor: col,
+    })
   }
 
   return (
     <div>
-      <div className="component">{name}</div>
-      <div style={myStyle}>Hello World</div>
-      <div>
-        <input type="button" value="click" onClick={buttonClick}></input>
+      <form id="myForm">
+        <div id="formArea">
+          <div id="lhs">
+            <input type="text" ref={text1} />
+            <input type="text" ref={text2} />
+
+            <select ref={comp}>
+              <option>RAM</option>
+              <option>CPU</option>
+              <option>GPU</option>
+            </select>
+          </div>
+          <div id="rhs">
+            <label>
+              <input type="radio" name="col" value="red" onChange={(e) => setCol(e.target.value)} />
+              red
+            </label>
+            <label>
+              <input type="radio" name="col" value="blue" onChange={(e) => setCol(e.target.value)} />
+              blue
+            </label>
+            <label>
+              <input type="radio" name="col" value="green" onChange={(e) => setCol(e.target.value)} />
+              green
+            </label>
+            <label>
+              <input type="radio" name="col" value="pink" onChange={(e) => setCol(e.target.value)} />
+              pink
+            </label>
+          </div>
+        </div>
+
+        <input type="submit" value="Submit" onClick={onSubmit} />
+        <input type="reset" value="Clear" />
+      </form>
+
+      <div style={style}>
+        <p>{text1.current ? text1.current.value : ''}</p>
+        <p>{text2.current ? text2.current.value : ''}</p>
+        <p>{comp.current ? comp.current.value : ''}</p>
       </div>
-      <div>{num}</div>
     </div>
   )
 }
